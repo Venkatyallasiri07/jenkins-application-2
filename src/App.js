e1d882900ff88c2ce3e1747a12b1271b82b0f499
Venkat <venkatyallasiri427@gmail.com>
2025-08-09 11:48:00 +0530

    changed application name

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Jenkins Application with node.js
         </a>
       </header>
       <p>
